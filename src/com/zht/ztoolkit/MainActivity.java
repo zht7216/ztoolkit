301ffb493db720a6d750cb419cdba0a7b2367c01
zht7216 <zht7216@163.com>
2013-09-22 18:32:37 +0800

    async imageloader

--- src/com/zht/ztoolkit/MainActivity.java
+++ src/com/zht/ztoolkit/MainActivity.java
@@ -1,7 +1,5 @@
 package com.zht.ztoolkit;
 
-import com.zht.ztoolkit.simplelistview.SimpleListView;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -10,15 +8,20 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import com.zht.ztoolkit.asyncimageloader.AsyncImageList;
+import com.zht.ztoolkit.simplelistview.SimpleListView;
+
 public class MainActivity extends Activity {
 
 	Button simpleListView;
+	Button asyncImageLoader;
 	Context mContext;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		simpleListView = (Button)findViewById(R.id.simple_listview);
+		asyncImageLoader = (Button)findViewById(R.id.async_imageloader);
 		init();
 	}
 
@@ -39,6 +42,17 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.setClass(mContext, SimpleListView.class);
+				getApplicationContext().startActivity(intent);
+			}
+		});
+		asyncImageLoader.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClass(mContext, AsyncImageList.class);
 				getApplicationContext().startActivity(intent);
 			}
 		});
---
+++ src/com/zht/ztoolkit/asyncimageloader/AsyncImageList.java
@@ -0,0 +1,52 @@
+package com.zht.ztoolkit.asyncimageloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.zht.ztoolkit.R;
+
+public class AsyncImageList extends Activity{
+	private GridView mGridView;
+	private String [] imageThumbUrls = ImageUrls.imageThumbUrls; 
+	private ImageAdapter mImageAdapter;
+	private FileUtils fileUtils;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.async_imagelist);
+		fileUtils = new FileUtils(this);
+		mGridView = (GridView)findViewById(R.id.gridView);
+		mImageAdapter = new ImageAdapter(this, mGridView, imageThumbUrls);
+		mGridView.setAdapter(mImageAdapter);
+	}
+
+	@Override
+	protected void onDestroy() {
+		mImageAdapter.cancelTask();
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add("删除手机中图片缓存");
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 0:
+			fileUtils.deleteFile();
+			Toast.makeText(getApplication(), "清空缓存成功", Toast.LENGTH_SHORT).show();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
